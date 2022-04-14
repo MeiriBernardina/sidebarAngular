dc22a72fcdeb54b0bbd8d66a992b76e7ec8eba69
Meiri Bernardina <meiri.umc@gmail.com>
2022-04-13 21:35:09 -0300

    first

---
+++ src/app/sidenav/sidenav.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { navbarData } from './nav-data';
+
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.scss']
+})
+export class SidenavComponent {
+
+
+  collapsed = false;
+  navData = navbarData;
+
+  toggleCollapse(): void {
+    this.collapsed = !this.collapsed;
+  }
+
+  closeSidenav(): void {
+    this.collapsed = false;
+  }
+
+}
